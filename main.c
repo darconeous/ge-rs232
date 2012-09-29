e7f9ca279a455bafba64d727b380d62ca61adbb0
Robert Quattlebaum <darco@deepdarc.com>
2012-09-29 13:44:04 -0700

    Print out the incoming packets for debugging purposes.

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 
 ge_rs232_status_t
 received_message(void* context, const uint8_t* data, uint8_t len,struct ge_rs232_s* instance) {
-	fprintf(stderr,"Got message, type %d\n",data[0]);
+	fprintf(stderr,"Got message: len:%d, type:%d { ",len,data[0]);
+	while(len--) {
+		fprintf(stderr,"%02X ",*data++);
+	}
+	fprintf(stderr,"}\n");
+
 	return GE_RS232_STATUS_OK;
 }
 
@@ -19,7 +24,9 @@ int main(int argc, const char* argv[]) {
 	ge_rs232_t interface = ge_rs232_init(&interface_data);
 	interface->received_message = &received_message;
 	interface->send_byte = &send_byte;
-	uint8_t byte;
+	uint8_t byte = GE_RS232_ATP_DYNAMIC_DATA_REFRESH;
+
+	ge_rs232_send_message(interface,&byte,1);
 
 	for(byte = fgetc(stdin);!feof(stdin);byte=fgetc(stdin)) {
 		ge_rs232_receive_byte(interface,byte);
